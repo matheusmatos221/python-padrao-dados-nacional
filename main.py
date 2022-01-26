c73d623889234f4a5130c8d4f974c38763ab60e6
Matheus Matos <matheusmatos.221@hotmail.com>
2022-01-26 16:57:32 -0300

    INSTALL and use validade_docbr

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+from validate_docbr import CPF
 from Cpf import Cpf
 
+cpf = CPF()
 
-cpf = "12345678912"
-cpf_novo = "11111111112"
-objeto_cpf = Cpf(cpf)
-objeto_cpf_novo = Cpf(cpf_novo)
-
-print(objeto_cpf)
-print(objeto_cpf_novo)
+print(cpf.validate(""))
